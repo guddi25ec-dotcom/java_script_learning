6ccc5d54e7196f5c005595b22ec705f43c58326f
Guddi Kumari <guddi25ec@gmail.com>
2026-06-19 15:45:16 +0530

    updated calculator programme

--- test.js
+++ test.js
@@ -1,12 +1,32 @@
-console.log("Calculator")
+//console.log("Calculator")
+
+// let a = 10;
+// let b = 20;
+
+// let sum = a + b;
+
+// console.log("Sum =", sum);
+// console.log("Addition =", a + b);
+// console.log("Subtraction =", a - b);
+// console.log("Multiplication =", a * b);
+// console.log("Division =", a / b);
+
+//console.log("Calculator");//
 
 let a = 10;
 let b = 20;
 
 let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+let remainder = a % b;
 
 console.log("Sum =", sum);
-console.log("Addition =", a + b);
-console.log("Subtraction =", a - b);
-console.log("Multiplication =", a * b);
-console.log("Division =", a / b);
+console.log("Subtraction =", difference);
+console.log("Multiplication =", product);
+console.log("Division =", quotient);
+console.log("Remainder =", remainder);
+
+console.log("Square of a =", a * a);
+console.log("Square of b =", b * b);
