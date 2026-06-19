5ab1d18bd11b212cf671e6db01779b7217da5de3
Guddi Kumari <guddi25ec@gmail.com>
2026-06-19 13:14:08 +0530

    Add calculator operations

--- test.js
+++ test.js
@@ -6,3 +6,7 @@ let b = 20;
 let sum = a + b;
 
 console.log("Sum =", sum);
+console.log("Addition =", a + b);
+console.log("Subtraction =", a - b);
+console.log("Multiplication =", a * b);
+console.log("Division =", a / b);
