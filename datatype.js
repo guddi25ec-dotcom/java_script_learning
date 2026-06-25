27b53796cce8664da4bfdd3d343dac80128a4681
Guddi Kumari <guddi25ec@gmail.com>
2026-06-25 13:10:17 +0530

    adding some features

---
+++ datatype.js
@@ -0,0 +1,16 @@
+let score=true
+
+console.log(typeof(score));
+
+console.log(true)
+
+
+console.log(typeof(score));
+
+let a = 10;
+
+console.log("Before increment:", a);
+
+++a;
+
+console.log("After increment:", a);
